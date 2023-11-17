bdaf2eca66418077d9ec24ce2d681548cdeec1eb
Molfar <sasha.prohor12@gmail.com>
2023-11-17 22:37:17 +0200

    main refactor

--- main.cpp
+++ main.cpp
@@ -30,17 +30,17 @@ int main(int argc, char *argv[])
         CardDAO cardDao = CardDAO::getInstance();
         TransactionDAO::getInstance();
 
-        userDao.addUser(new User(1, "Frdy", "Fzber", "+1987"));
-        //User* user = userDao.getById(0);
-        //User* user2 = userDao.getByPhoneNum("+1987");
-
-        cardDao.addCard(new CreditCard(0,"1", "111", 1, 10, QDate::currentDate(), 0.01, 0.01, 100, "1111"));
-
-        ATM::getInstance().insertCard(cardDao.getById(1));
-
-        ATM::getInstance().withdrawMoney(10000);
-
-        std::cout << ATM::getInstance().getAvailableWithdraw() << std::endl;
+//        userDao.addUser(new User(1, "Frdy", "Fzber", "+1987"));
+//        //User* user = userDao.getById(0);
+//        //User* user2 = userDao.getByPhoneNum("+1987");
+//
+//        cardDao.addCard(new CreditCard(1,"1", "111", 1, 10, QDate::currentDate(), 0.01, 0.01, 100, "1111"));
+//
+//        ATM::getInstance().insertCard(cardDao.getById(1));
+//
+//        ATM::getInstance().withdrawMoney(10000);
+//
+//        std::cout << ATM::getInstance().getAvailableWithdraw() << std::endl;
 //        cardDao.addCard(0, "1987", "111", 0, 14.88,
 //                        QDate::currentDate(), 0, 0.1,
 //                        0.3, 0,"123");
