ffaf8c3d5de66ffde1375bf8cf48ee6ec4712361
Molfar <sasha.prohor12@gmail.com>
2023-11-19 00:38:13 +0200

    FINAL COMMIT 😈

--- main.cpp
+++ main.cpp
@@ -141,34 +141,6 @@ int main(int argc, char *argv[])
     } else {
         // trigger initializations after the db connection is established
         initDB();
-
-//        userDao.addUser(new User(1, "Frdy", "Fzber", "+1987"));
-//        //User* user = userDao.getById(0);
-//        //User* user2 = userDao.getByPhoneNum("+1987");
-//
-//        cardDao.addCard(new CreditCard(1,"1", "111", 1, 10, QDate::currentDate(), 0.01, 0.01, 100, "1111"));
-//
-//        ATM::getInstance().insertCard(cardDao.getById(1));
-//
-//        ATM::getInstance().withdrawMoney(10000);
-//
-//        std::cout << ATM::getInstance().getAvailableWithdraw() << std::endl;
-//        cardDao.addCard(0, "1987", "111", 0, 14.88,
-//                        QDate::currentDate(), 0, 0.1,
-//                        0.3, 0,"123");
-//        cardDao.addCard(1, "1983", "111", 0, 14.88,
-//                        QDate::currentDate(), 1, 0.1,
-//                        0.3, 0,"123");
-//
-        User* user = UserDAO::getInstance().getById(1);
-        Card* card1 = CardDAO::getInstance().getByUserId(2);
-        DebitCard* debit = dynamic_cast<DebitCard*>(card1);
-        CardDAO::getInstance().UpdateCard(*debit);
-        std::cout << (user ? user->ToString().toStdString() : "") << std::endl;
-        std::cout << (card1 ? card1->ToString().toStdString() : "") << std::endl;
-        QList<Card*> cards = CardDAO::getInstance().getAllUserCards(3);
-        std::for_each(cards.begin(), cards.end(), [](Card* card){
-            std::cout << card->ToString().toStdString() << std::endl;
         });
     }
 
